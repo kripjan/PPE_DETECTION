2209f0f7f2c359807c424461824f9e875e11c947
kripjan <krijansthaa@gmail.com>
2024-10-30 09:42:43 +0545

    added codes

--- app/login_bp_folder/__init__.py
+++ app/login_bp_folder/__init__.py
@@ -1,5 +1,5 @@
 # app/auth/__init__.py
 from flask import Blueprint
 
-login_bp = Blueprint('login_bp', __name__)
+login_bp = Blueprint('login_bp', __name__, static_folder='static', template_folder='templates')
 from .main import routes  # Import routes to register them with the blueprint
