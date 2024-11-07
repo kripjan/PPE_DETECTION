05fc6b81b88972a26db2bed1b0e33355ce361acb
kripjan <krijansthaa@gmail.com>
2024-11-07 21:17:51 +0545

    created homepage and profilepage

--- app/login_bp_folder/__init__.py
+++ app/login_bp_folder/__init__.py
@@ -3,3 +3,8 @@ from flask import Blueprint
 
 login_bp = Blueprint('login_bp', __name__, template_folder='templates', static_folder='static')
 from .main import routes  # Import routes to register them with the blueprint
+
+
+#mathi ko login_bp variable banako lai aba jata bata ni access garna milcha ani organize garna sajilo vayo aba
+
+#4th line ma blueprint('') vanney constructor cha jalse    variable = object from bluepront class 
